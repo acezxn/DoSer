6cbc7f7a9f647ada14c31f7b1c583c0c9c100529
acezxn <acezxn@gmail.com>
2022-07-28 17:45:00 +0800

    update

--- DoSer.py
+++ DoSer.py
@@ -4,7 +4,19 @@ Scripts.UDPFlooder as UDPFlooder, \
 Scripts.ICMPFlooder as ICMPFlooder, \
 Scripts.Smurf as Smurf, Scripts.Teardropper as Teardropper
 
+def intInput(q):
+    while True:
+        res = input(q)
+        try:
+            return int(res)
+        except:
+            print("Please input a number")
+
 def list_choice(choices=list()):
+    banner = """
+    Implementation of multiple DoS techniques
+    For more info, please read README.md
+    """
     menu = """
     Options:
     1) SYN flood
@@ -13,9 +25,12 @@ def list_choice(choices=list()):
     4) Smurf attack
     5) Deauthentication
     6) Teardrop
+
+    Please input a number
     """
 
     if len(choices) == 0:
+        print(banner)
         print(menu)
 
 
@@ -24,32 +39,36 @@ if __name__ == "__main__":
     choices = []
     list_choice(choices)
 
-    res = input(">> ")
-    choices.append(int(res))
+    res = intInput(">> ")
+    choices.append(res)
     
     if choices[0] == 1:
         print("Insert target IP")
         target_ip = input(">> ")
         print("Insert port")
-        port = int(input(">> "))
+        port = intInput(">> ")
 
+        print("Performing SYN flooding attack")
         SYNFlooder.flood(target_ip=target_ip, target_port=port)
     elif choices[0] == 2:
         print("Insert target IP")
         target_ip = input(">> ")
         print("Insert port")
-        port = int(input(">> "))
+        port = intInput(">> ")
 
+        print("Performing UDP flooding attack")
         UDPFlooder.flood(target_ip=target_ip, target_port=port)
     elif choices[0] == 3:
         print("Insert target IP")
         target_ip = input(">> ")
 
+        print("Performing ICMP flooding attack")
         ICMPFlooder.flood(target_ip=target_ip)
     elif choices[0] == 4:
         print("Insert target IP")
         target_ip = input(">> ")
 
+        print("Performing Smurf attack")
         Smurf.attack(target_ip=target_ip)
     
     elif choices[0] == 5:
@@ -62,10 +81,12 @@ if __name__ == "__main__":
         print("Insert packet count to send")
         count = input(">> ")
 
+        print("Performing Deauthentication attack")
         Deauther.deauth(target_mac=target_mac, gateway_mac=gateway_mac, interface=iface, count=count)
 
     elif choices[0] == 6:
         print("Insert target IP")
         target_ip = input(">> ")
-
+        
+        print("Performing Teardrop attack")
         Teardropper.attack(target_ip=target_ip)
